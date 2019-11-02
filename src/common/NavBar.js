3c98ed13f35d944e620f16e3ba38375e27abc4c9
pwang86 <pwang86@gmail.com>
2019-11-02 18:27:25 +1100

    remove logout from topright corner

--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
-import { redirect } from "./helper";
-import axios from "axios";
+/* import { redirect } from "./helper";
+import axios from "axios"; */
 
-function handleLogout() {
+/* function handleLogout() {
   axios.defaults.headers.common.Authorization = null;
   localStorage.clear();
   redirect("/login");
-}
+} */
 
 function NavBar() {
   return (
@@ -18,13 +18,14 @@ function NavBar() {
           Stock Location System
         </Link>
       </div>
-      <div className="navbar-menu">
+
+      {/*       <div className="navbar-menu">
         <div className="navbar-end">
           <a className="navbar-item" onClick={handleLogout}>
             Logout
           </a>
         </div>
-      </div>
+      </div> */}
     </nav>
   );
 }
