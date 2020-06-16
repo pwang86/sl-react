24b08678863c1ec8e8c7e40d2fb0caa0917c4a65
pwang86 <pwang86@gmail.com>
2020-06-16 10:23:20 +1000

    update center pageloader

--- src/common/PageLoader.js
+++ src/common/PageLoader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PageLoader() {
   return (
-    <div className="has-text-center">
+    <div className="has-text-centered">
       <i className="fas fa-spinner fa-pulse fa-3x" />
     </div>
   );
