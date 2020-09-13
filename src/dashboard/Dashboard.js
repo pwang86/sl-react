c2213f33e05393fac15fdd4d53083385f6bda7f1
pwang86 <pwang86@gmail.com>
2020-09-13 18:52:37 +1000

    change dashboard to class

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -2,57 +2,74 @@ import React from "react";
 import MainContent from "../common/MainContent";
 import "./Dashboard.scss";
 
-function Dashboard() {
-  return (
-    <MainContent>
-      <h1 className="title sl-dashboard__title has-text-grey">
-        {" "}
-        Welcome to Stock Location System
-      </h1>
-      <div className="tile is-ancestor">
-        <div className="tile is-6 is-parent">
-          <div className="tile is-child box notification is-info">
-            <p className="title">Stock Location</p>
-            <div className="content">All locations in the warehouse</div>
+class Dashboard extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      showSuccess: false,
+      error: "",
+    };
+  }
+
+  handleImport = () => {};
+
+  render() {
+    return (
+      <MainContent>
+        <h1 className="title sl-dashboard__title has-text-grey">
+          {" "}
+          Welcome to Stock Location System
+        </h1>
+        <div className="tile is-ancestor">
+          <div className="tile is-6 is-parent">
+            <div className="tile is-child box notification is-info">
+              <p className="title">Stock Location</p>
+              <div className="content">All locations in the warehouse</div>
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <div className="tile is-child box notification is-success">
+              <p className="title">Stock Taking</p>
+              <div className="content">One click to get all results</div>
+            </div>
           </div>
         </div>
-        <div className="tile is-parent">
-          <div className="tile is-child box notification is-success">
-            <p className="title">Stock Taking</p>
-            <div className="content">One click to get all results</div>
-          </div>
-        </div>
-      </div>
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <div className="tile is-child box notification is-danger">
-            <p className="title">VR1600V?</p>
-            <div className="content">import from excel template</div>
-            <div className="content file has-name">
-              <label className="file-label">
-                <input className="file-input" type="file" name="excel" />
-                <span className="file-cta">
-                  <span className="file-icon">
-                    <i className="fas fa-upload" />
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box notification is-primary">
+              <p className="title">VR1600V?</p>
+              <div className="content">import from excel template</div>
+              <div className="content file has-name">
+                <label className="file-label">
+                  <input
+                    className="file-input"
+                    type="file"
+                    name="excel"
+                    onChange={this.handleImport}
+                  />
+                  <span className="file-cta">
+                    <span className="file-icon">
+                      <i className="fas fa-upload" />
+                    </span>
+                    <span className="file-label">Choose a file…</span>
                   </span>
-                  <span className="file-label">Choose a file…</span>
-                </span>
-                <span className="file-name">No file chosen</span>
-              </label>
-            </div>
-            <div className="content">
-              <button className="button is-danger dashboard-btn">
-                <svg>
-                  <rect className="svg-rect" x="0" y="0" />
-                </svg>
-                Start process
-              </button>
+                  <span className="file-name">No file chosen</span>
+                </label>
+              </div>
+              <div className="content">
+                <button className="button is-primary dashboard-btn">
+                  <svg>
+                    <rect className="svg-rect" x="0" y="0" />
+                  </svg>
+                  Start process
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </MainContent>
-  );
+      </MainContent>
+    );
+  }
 }
 
 export default Dashboard;
